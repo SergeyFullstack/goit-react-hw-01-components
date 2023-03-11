a50fb907f0ef96f304fc8fa907f7e34918c72399
SergeyFullstack <111132907+SergeyFullstack@users.noreply.github.com>
2023-03-11 23:26:17 +0200

    Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,6 @@ import { Profile } from 'components/Profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics';
 import { FriendList } from 'components/Friends/FriendList';
 import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
-
 import user from 'components/data/user.json';
 import data from 'components/data/data.json';
 import friends from 'components/data/friends.json';
