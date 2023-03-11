c31dd9de1bd7710e2e77c1e6f21d6aeef7bdd611
SergeyFullstack <111132907+SergeyFullstack@users.noreply.github.com>
2023-03-11 22:53:08 +0200

    Update

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import {
+  StatisticsList,
+  StatisticsSection,
+  StatisticsTitle,
+} from 'components/Statistics/Statistics.styled';
+import { StatisticsItem } from 'components/Statistics/StatisticsItem';
+
+export const Statistics = ({ title, stats }) => {
+  return (
+    <StatisticsSection>
+      <StatisticsTitle>{title}</StatisticsTitle>
+
+      <StatisticsList>
+       {<StatisticsItem stats={stats} />}
+      </StatisticsList>
+    </StatisticsSection>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+};
+
---
+++ src/components/Statistics/Statistics.styled.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+export const StatisticsSection = styled.section`
+  width: 300px;
+  margin: 0 auto;
+  margin-bottom: 40px;
+  background-color: #fff;
+  overflow: hidden;  
+  background-color: #fff;
+  box-shadow: 10px 10px 20px rgb(150, 150, 150);
+`;
+
+export const StatisticsTitle = styled.h2`
+  padding: 20px 0;
+  text-align: center;
+  color: grey;
+`;
+
+export const StatisticsList = styled.ul`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const StatisticsItemStyle = styled.li`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex-basis: 20%;
+  background-color: ${({ backgrounds }) => backgrounds};
+
+  padding: 10px 0;
+  font-weight: 500;
+`;
