bd697d73ccab18fe37d771f124d69029d80df4f3
SergeyFullstack <111132907+SergeyFullstack@users.noreply.github.com>
2023-05-20 15:57:50 +0300

    Update

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -37,12 +37,12 @@ const TransactionItem = ({ items }) => {
 
 
 TransactionHistory.propTypes = {
-  items: PropTypes.arrayOf(
+    items: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
       type: PropTypes.string.isRequired,
       amount: PropTypes.string.isRequired,
       currency: PropTypes.string.isRequired,
-    }).isRequired
-  ).isRequired,
+    })
+  ),
 };
